fb19e02dd05217fedd42f777ab86c631b2befe37
0966474398 <59565274+0966474398@users.noreply.github.com>
2020-05-19 13:01:59 +0700

    Dich commit

---
+++ tabbarController/tabbarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabbarController
+//
+//  Created by Nguyen Quoc Dich on 5/19/20.
+//  Copyright © 2020 Nguyen.Quoc.Dich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
